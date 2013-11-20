7c8926be7b3a5f28634d626e26485ed6851f78c7
Bertie Wheen <wheen.b@gmail.com>
2013-11-20 01:20:00 +0000

    Changed the arguments the program takes
    Used to be a list of machine codes, now it's:
    DCPU code_file [-printStart] [-printEnd]

--- DCPU/main.cpp
+++ DCPU/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <sstream>
+#include <fstream>
 #include "DCPU.h"
 #include "LEM1802.h"
 #include "HMD2043.h"
@@ -7,42 +7,74 @@
 
 int main(int argc, char *argv[])
 {
-	DCPU computer;
-	// Load args into RAM
-	std::cout << "Loading " << std::dec << argc - 1 << " instructions into RAM...";
-	std::stringstream ss;
-	for(int i = 1; i < argc && i <= RAM_SIZE; ++i)
+	using std::ios;
+	// Check that the correct number of arguments were given
+	if(argc < 2)
 	{
-		ss << argv[i];
-		ss >> computer.RAM[i - 1];
-		ss.clear();
+		std::cout << "ERROR: Insufficient arguments provided." << std::endl;
+		std::cout << "USAGE:" << std::endl;
+		std::cout << "    DCPU code_file [-printStart] [-printEnd]" << std::endl;
+		return 1;
 	}
-	std::cout << "Done." << std::endl;
+	// "Create" the computer (if only it were that easy)
+	DCPU computer;
+	// Load the program
+	unsigned int numInstructions = 0;
+	std::ifstream file(argv[1], ios::in | ios::binary | ios::ate);
+	if(file.is_open())
+	{
+		std::ifstream::pos_type size = file.tellg();
+		char *tempArray = new char[(unsigned int)size];
+		file.seekg(0, ios::beg);
+		file.read(tempArray, size);
+		file.close();
+		for(int i = 0; i < RAM_SIZE && i < size/2; i++)
+		{
+			char upper = tempArray[i*2];
+			char lower = tempArray[i*2 + 1];
+			computer.RAM[i] = (upper << 8) | lower;
+			numInstructions = i + 1;
+		}
+		delete[] tempArray;
+	}
+	else
+	{
+		std::cout << "ERROR: Unable to read code file." << std::endl;
+		return 1;
+	}
 	// Connect hardware
 	LEM1802 lem;
 	HMD2043 hmd;
 	HMU1440 hmu;
 	hmd.loadDisk(&hmu);
 #define CONNECT_HARDWARE(x) std::cout << "Connecting " << x.toString() << std::endl; \
-                            computer.connectHardware(&x);
-	CONNECT_HARDWARE(lem)
-	CONNECT_HARDWARE(hmd)
+                            computer.connectHardware(&x)
+	CONNECT_HARDWARE(lem);
+	CONNECT_HARDWARE(hmd);
 #undef CONNECT_HARDWARE
 	// Print initial state
-	std::cout << "DCPU state:" << std::endl;
-	computer.printState();
+	std::string printStart("-printStart");
+	if((argc >= 3 && printStart == argv[2])
+	|| (argc >= 4 && printStart == argv[3])) {
+		std::cout << "Initial DCPU state:" << std::endl;
+		computer.printState();
+	}
 	// Execute
-	std::cout << "Running " << std::dec << argc - 1 << " instructions...";
-	computer.run(argc - 1);
+	std::cout << "Running " << std::dec << numInstructions << " instructions...";
+	computer.run(numInstructions);
 	std::cout << "Done." << std::endl;
 	// Print final state
-	std::cout << "DCPU state:" << std::endl;
-	computer.printState();
+	std::string printEnd("-printEnd");
+	if((argc >= 3 && printEnd == argv[2])
+	|| (argc >= 4 && printEnd == argv[3])) {
+		std::cout << "Final DCPU state:" << std::endl;
+		computer.printState();
+	}
 	// Disconnect hardware
 #define DISCONNECT_HARDWARE(x) std::cout << "Disconnecting " << x.toString() << std::endl; \
-                               computer.disconnectHardware(&x);
-	DISCONNECT_HARDWARE(lem)
-	DISCONNECT_HARDWARE(hmd)
+                               computer.disconnectHardware(&x)
+	DISCONNECT_HARDWARE(lem);
+	DISCONNECT_HARDWARE(hmd);
 #undef DISCONNECT_HARDWARE
 	return 0;
 }
